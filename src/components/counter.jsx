ede5cb59596b6209b5cd94a79e56111b2056a227
ozan isik <ozan_isik14@hotmail.com>
2022-01-03 22:42:44 -0500

    Conditional Rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,30 +8,27 @@ class Counter extends Component {
         tags: ['tag1', 'tags2', 'tags3']
     };
 
+    renderTags() {
+        if(this.state.tags.length === 0){
+            return <p>There are no tags</p>
+        } else {
+            return ( <ul>
+                        { this.state.tags.map(tag => <li key={ tag } >{ tag }</li>) }
+                    </ul>
+            )
+        }
+    }
+
     render() {
         // this is a jsx syntax
         // jsx expression must have only one element
         // attributes for class are different in jsx. They are called className
         return (
-        <React.Fragment>
-            <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                { this.state.tags.map(tag => <li key={ tag } >{ tag }</li>) }
-            </ul>
-        </React.Fragment>
+        <div>
+            { this.state.tags.length === 0 && "please create a new tag!" }
+            { this.renderTags() }
+        </div>
         );
-    }
-
-    getBadgeClasses(){
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-    formatCount(){
-        const { count } = this.state;
-        return count === 0 ? 'zero' : count;
     }
 }
 
