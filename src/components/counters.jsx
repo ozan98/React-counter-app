64bab10ba8050804914f18bcc974008749781f80
ozan isik <ozan_isik14@hotmail.com>
2022-01-04 13:43:40 -0500

    Single Source of Truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,6 +10,14 @@ class Counters extends React.Component {
             { id: 4, value: 0 }
         ]
     }
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0
+            return c
+        })
+        this.setState({ counters : counters })
+    }
+
     handleDelete = (counterId) => {
         const counters = this.state.counters.filter(c => c.id !== counterId)
         this.setState({ counters: counters })
@@ -18,6 +26,9 @@ class Counters extends React.Component {
     render() { 
         return (
         <div>
+            <button
+                onClick={ this.handleReset }
+                 className="btn btn-primary btn-sm m-2">Reset</button>
             { this.state.counters.map(counter => 
                 <Counter 
                     key={ counter.id }
